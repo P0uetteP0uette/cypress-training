78167b7f877e006fb757dfb329d3268f64d80cab
Nathan LYONNET <nathan.lyonnet@et.esiea.fr>
2026-07-10 11:56:50 +0200

    test de la CI

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,7 @@
 describe('Ma session d\'entraînement QA', () => {
 
   it('Vérifie le message d\'erreur avec de mauvais identifiants', () => {
-    // 1. Aller sur le site
+    // 1. Aller sur le site 
     cy.visit('https://www.saucedemo.com/')
     cy.get('[data-test="username"]').type('zebi')
     cy.get('[data-test="password"]').type('bjr')
